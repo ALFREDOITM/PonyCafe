87ec423d82d0f0c0ead794417e4a263dce15e472
ALFREDOITM <alfredosanchez.as68@gmail.com>
2022-10-11 00:20:32 -0500

    mostrar item seleccionado funcional, pendiente por hacerlo mas estetico

--- app/src/main/java/org/example/ponycafe/SelectedItemActivity.kt
+++ app/src/main/java/org/example/ponycafe/SelectedItemActivity.kt
@@ -1,4 +1,39 @@
 package org.example.ponycafe
 
-class SelectedItemActivity {
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
+
+class SelectedItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selected_item)
+
+        val foodInfo: Array<JSONObject> = arrayOf(
+            JSONObject("{\"comida\": \"Chilaquiles\", \"descripcion\": \"Tortilla de maiz triangular bañada en salsa acompañado de una guarnicion\"}"),
+            JSONObject("{\"comida\": \"Enfrijoladas\", \"descripcion\": \"Tortilla de maiz cubierta en salsa de frijol con relleno de queso y cebolla\"}"),
+            JSONObject("{\"comida\": \"Sopes\", \"descripcion\": \"Disco de masa de maiz con relleno a elegir\"}"),
+            JSONObject("{\"comida\": \"Tacos\", \"descripcion\": \"Tortilla de maiz con relleno de proteina carnica a elegir\"}"),
+            JSONObject("{\"comida\": \"Pizza\", \"descripcion\": \"Disco con rebanadas triangulares las cuales vienen en una caja cuadrada\"}")
+        )
+
+       val item = intent.getIntExtra("item",1)
+
+        if (item == 0)
+            findViewById<ImageView>(R.id.itemIV1).setImageResource(R.drawable.chilaquiles)
+        else if (item == 1)
+            findViewById<ImageView>(R.id.itemIV1).setImageResource(R.drawable.enfrijoladas)
+        else if (item == 2)
+            findViewById<ImageView>(R.id.itemIV1).setImageResource(R.drawable.sopes)
+        else if (item == 3)
+            findViewById<ImageView>(R.id.itemIV1).setImageResource(R.drawable.taco)
+        else if (item == 4)
+            findViewById<ImageView>(R.id.itemIV1).setImageResource(R.drawable.pizza)
+
+        findViewById<TextView>(R.id.itemTV1).text = foodInfo[item].getString("comida")
+        findViewById<TextView>(R.id.itemTV2).text = foodInfo[item].getString("descripcion")
+
+    }
 }
--- app/src/main/java/org/example/ponycafe/StoreMainActivity.kt
+++ app/src/main/java/org/example/ponycafe/StoreMainActivity.kt
@@ -1,5 +1,6 @@
 package org.example.ponycafe
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.widget.AdapterView
@@ -16,22 +17,20 @@ class StoreMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_store_main)
 
-        val fakeVideoData: Array<JSONObject> = arrayOf(
-            JSONObject("{\"comida\": \"Chilaquiles\", \"Descripción\": \"Tortilla de maiz cubierta de salsa con relleno de papa con zanahoria\"}"),
-            JSONObject("{\"comida\": \"Enfrijoladas\", \"Descripción\": \"Tortilla de maiz cubierta en salsa de frijol con relleno de queso y cebolla\"}"),
-            JSONObject("{\"comida\": \"Sopes\", \"Descripción\": \"Disco de masa de maiz con relleno a elegir\"}"),
-            JSONObject("{\"comida\": \"Tacos\", \"Descripción\": \"Tortilla de maiz con relleno de proteina carnica a elegir\"}"),
-            JSONObject("{\"comida\": \"Pizza\", \"Descripción\": \"Disco con rebanadas triangulares las cuales vienen en una caja cuadrada\"}")
-        )
         //Codigo relacionado al gridview
         courseGRV = findViewById(R.id.idGRV)
         val courseAdapter = obtenerElementosGridView()
         courseGRV.adapter = courseAdapter
         courseGRV.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            /*
             Toast.makeText(
                 applicationContext, courseList[position].courseName + " selected",
                 Toast.LENGTH_SHORT
             ).show()
+            */
+            val intent = Intent(this, SelectedItemActivity::class.java)
+            intent.putExtra("item", position)
+            startActivityForResult(intent,1)
         }
     }
 
