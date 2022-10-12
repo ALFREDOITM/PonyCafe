6fe723be90c6a6434df2d2476d234c0a787ad252
naarsee <narseg.f@gmail.com>
2022-10-12 18:44:35 -0500

    user data activity with clikabl false

--- app/src/main/java/org/example/ponycafe/UserProfile.kt
+++ app/src/main/java/org/example/ponycafe/UserProfile.kt
@@ -1,11 +1,17 @@
 package org.example.ponycafe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class UserProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+    }
+    fun launchLogin(view: View?) {
+        val start = Intent(this, MainActivity::class.java)
+        startActivity(start)
     }
 }
