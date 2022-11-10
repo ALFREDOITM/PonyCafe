08e5c7ee94fcee8daa2f53b2ea708348deafa33d
ALFREDOITM <alfredosanchez.as68@gmail.com>
2022-11-10 14:19:56 -0600

    Cambios para enviar datos a firebase

--- app/src/main/java/org/example/ponycafe/NewUserActivity.kt
+++ app/src/main/java/org/example/ponycafe/NewUserActivity.kt
@@ -6,18 +6,23 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import org.example.ponycafe.databinding.ActivityNewUserBinding
 import java.util.*
 
 class NewUserActivity : AppCompatActivity() {
-    var id=0
+
     private lateinit var viewBinding: ActivityNewUserBinding
+    private lateinit var database : DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding=ActivityNewUserBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
         val userTypes=ArrayAdapter<String>(this,
             android.R.layout.simple_spinner_dropdown_item)
 
@@ -26,9 +31,6 @@ class NewUserActivity : AppCompatActivity() {
 
         val questions2=ArrayAdapter<String>(this,
             android.R.layout.simple_spinner_dropdown_item)
-
-        val database=Firebase.database
-        val myRef=database.reference
 
         questions1.addAll((Arrays.asList("Selecciona una pregunta","¿En qué lugar nació tu madre?",
         "¿En qué primaria ibas?",
@@ -46,20 +48,37 @@ class NewUserActivity : AppCompatActivity() {
 
         userTypes.addAll(Arrays.asList("Selecciona un tipo de usuario","Estudiante","Personal del Plantel" ))
         viewBinding.spinUsertype.adapter=userTypes
-    }
 
-    fun launchLogin(view: View?) {
-        val start = Intent(this, MainActivity::class.java)
-        startActivity(start)
-        registerUser()
+        viewBinding.btnRegister.setOnClickListener{
+            registerUser()
+        }
     }
 
     fun registerUser(){
-        val database=Firebase.database
-        val myRef=database.reference
+        val name = viewBinding.etName.text.toString()
+        val fatherLN = viewBinding.etFatherLastName.text.toString()
+        val motherLN = viewBinding.etMotherLastName.text.toString()
+        val bDate = viewBinding.calendarView.date.toString()
+        val user = viewBinding.etUser.text.toString()
+        val pass = viewBinding.etPassword.text.toString()
+        val type = viewBinding.spinUsertype.selectedItem.toString()
+        val email = viewBinding.etEmail.text.toString()
+        val quest1 = viewBinding.spinSecureQuestions.selectedItem.toString()
+        val answ1 = viewBinding.etAnswer.text.toString()
+        val quest2 = viewBinding.spinSecureQuestions2.selectedItem.toString()
+        val answ2 = viewBinding.etAnswer2.text.toString()
 
-        myRef.child("users").child(id.toString()).setValue(NewUser("Alfredo", "Sanchez","Izquierdo","alfre", "pass123","alfred@pony.com"))
-        id += 1
-        Toast.makeText(this, "usuario creado con exito",Toast.LENGTH_SHORT).show();
+        database = FirebaseDatabase.getInstance().getReference("users")
+        val User = User(name, fatherLN, motherLN, bDate, user, pass, type, email, quest1, answ1, quest2, answ2)
+        database.child(user).setValue(User).addOnSuccessListener {
+            Toast.makeText(this, "usuario creado con exito",Toast.LENGTH_SHORT).show();
+            val start = Intent(this, MainActivity::class.java)
+            startActivity(start)
+        }.addOnFailureListener{
+            Toast.makeText(this, "Error",Toast.LENGTH_SHORT).show();
+        }
+
+        //myRef.child("users").child(id.toString()).setValue(NewUser("Alfredo", "Sanchez","Izquierdo","alfre", "pass123","alfred@pony.com"))
+        //id += 1
     }
 }
---
+++ app/src/main/java/org/example/ponycafe/User.kt
@@ -0,0 +1,8 @@
+package org.example.ponycafe
+
+data class User(val name : String? = null, val fatherLN : String? = null,val motherLN : String? = null, val birth : String? = null,
+                val user : String? = null, val pass : String? = null, val type : String? = null, val email : String? = null,
+                val question1 : String? = null, val answer1 : String? = null, val question2 : String? = null, val answer2 : String? = null,){
+    //Esta clase sirve para administrar los datos a subir
+
+}
