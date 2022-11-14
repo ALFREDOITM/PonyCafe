3041d261c69810da6e2a58cd370a919b1b698853
ALFREDOITM <alfredosanchez.as68@gmail.com>
2022-11-14 00:06:26 -0600

    Agregado de menu en modo admin

--- app/src/main/java/org/example/ponycafe/AdminActivity.kt
+++ app/src/main/java/org/example/ponycafe/AdminActivity.kt
@@ -2,10 +2,33 @@ package org.example.ponycafe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
 
 class AdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_admin, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.shopping_cart -> {
+                //lanzarShoppingCart()
+                true
+            }
+            R.id.user_profile -> {
+                //lanzarUserProfile()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+
+        }
     }
 }
