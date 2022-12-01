0df5af4e92a4d885060c82c72622803572a8aca9
naarsee <narseg.f@gmail.com>
2022-12-01 14:33:16 -0600

    login funcional

--- app/src/main/java/org/example/ponycafe/LoginActivity.kt
+++ app/src/main/java/org/example/ponycafe/LoginActivity.kt
@@ -1,52 +1,56 @@
 package org.example.ponycafe
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import com.google.firebase.auth.FirebaseAuth
+import org.example.ponycafe.R
+import org.example.ponycafe.StoreMainActivity
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Theme_PonyCafe)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
         setup()
     }
 
-    private fun setup() {
+    private fun setup(){
         findViewById<Button>(R.id.btnLogin).setOnClickListener{
-
-            val email=findViewById<EditText>(R.id.etEmail).text.toString()
-            val pass=findViewById<EditText>(R.id.etPass).text.toString()
-            Toast.makeText(this,email + pass, Toast.LENGTH_SHORT).show()
-
+            //Toast.makeText(this,"hola", Toast.LENGTH_SHORT).show()
+            if(findViewById<EditText>(R.id.etEmail).text.isNotEmpty() && findViewById<EditText>(R.id.etPass).text.isNotEmpty()){
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(findViewById<EditText>(R.id.etEmail).text.toString(),
+                        findViewById<EditText>(R.id.etPass).text.toString()).addOnCompleteListener {
+                        if(it.isSuccessful){
+                            launchHome(it.result?.user?.email ?: "")
+                        }else{
+                            alert()
+                        }
+                    }
+            }
         }
     }
-
-    private fun launchHome(email: String) {
+    private fun launchHome(email:String){
         val mainIntent=Intent(this, StoreMainActivity::class.java).apply {
             putExtra("email",email)
         }
         startActivity(mainIntent)
-
     }
+
     private fun alert(){
-        val builder = AlertDialog.Builder(this)
+        val builder =AlertDialog.Builder(this)
         builder.setTitle("Error")
         builder.setMessage("Datos erroneos")
         builder.setPositiveButton("Aceptar", null)
-        val dialog: AlertDialog =builder.create()
+        val dialog: AlertDialog=builder.create()
         dialog.show()
     }
-
     fun launchRegister(view: View?) {
         val start = Intent(this, NewUserActivity::class.java)
         startActivity(start)
@@ -56,7 +60,3 @@ class MainActivity : AppCompatActivity() {
         startActivity(start)
     }
 }
-
-    private fun launchHome(s: String) {
-
-    }
