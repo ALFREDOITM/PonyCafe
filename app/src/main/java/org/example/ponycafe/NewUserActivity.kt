20d776bff246c44618638da091f4cd19ab44a2ce
ALFREDOITM <alfredosanchez.as68@gmail.com>
2022-12-01 12:49:35 -0600

    registro de usuario en auth y en database

--- app/src/main/java/org/example/ponycafe/NewUserActivity.kt
+++ app/src/main/java/org/example/ponycafe/NewUserActivity.kt
@@ -3,8 +3,10 @@ package org.example.ponycafe
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import org.example.ponycafe.databinding.ActivityNewUserBinding
@@ -14,12 +16,13 @@ class NewUserActivity : AppCompatActivity() {
 
     private lateinit var viewBinding: ActivityNewUserBinding
     private lateinit var database : DatabaseReference
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding=ActivityNewUserBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-
+        auth = FirebaseAuth.getInstance()
         val userTypes=ArrayAdapter<String>(this,
             android.R.layout.simple_spinner_dropdown_item)
 
@@ -47,17 +50,37 @@ class NewUserActivity : AppCompatActivity() {
         viewBinding.spinUsertype.adapter=userTypes
 
         viewBinding.btnRegister.setOnClickListener{
-            registerUser()
+            registerUserAuth()
         }
     }
-
-    private fun registerUser(){
+    private fun registerUserAuth(){
+        val email = viewBinding.etEmail.text.toString()
+        val pass = viewBinding.etPassword.text.toString()
+        auth.createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    val uid = auth.uid.toString()
+                    //updateUI(user)
+                    registerUser(uid)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Usuario existente / Contraseña debil",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
+    }
+    private fun registerUser(uid: String) {
         val name = viewBinding.etName.text.toString()
         val fatherLN = viewBinding.etFatherLastName.text.toString()
         val motherLN = viewBinding.etMotherLastName.text.toString()
         val bDate = viewBinding.calendarView.date.toString()
         val user = viewBinding.etUser.text.toString().uppercase(Locale.getDefault())
-        val pass = viewBinding.etPassword.text.toString()
+        val pass = "a2352bhbjhj"
         val type = viewBinding.spinUsertype.selectedItem.toString()
         val email = viewBinding.etEmail.text.toString()
         val quest1 = viewBinding.spinSecureQuestions.selectedItem.toString()
@@ -67,7 +90,7 @@ class NewUserActivity : AppCompatActivity() {
 
         database = FirebaseDatabase.getInstance().getReference("users")
         val User = User(name, fatherLN, motherLN, bDate, user, pass, type, email, quest1, answ1, quest2, answ2)
-        database.child(user).setValue(User).addOnSuccessListener {
+        database.child(uid).setValue(User).addOnSuccessListener {
             Toast.makeText(this, "usuario creado con exito",Toast.LENGTH_SHORT).show()
             val start = Intent(this, MainActivity::class.java)
             startActivity(start)
